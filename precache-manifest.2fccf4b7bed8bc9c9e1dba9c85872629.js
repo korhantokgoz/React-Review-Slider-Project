64d2f7577be2a300de7587782ed89fafdee949f8
Sam Smith <korhan.tokgoz92@gmail.com>
2022-06-21 12:16:22 +0300

    Updates

---
+++ precache-manifest.2fccf4b7bed8bc9c9e1dba9c85872629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5fad96569dcd1494ed28018d92f993",
+    "url": "/React-Review-Slider-Project/index.html"
+  },
+  {
+    "revision": "cc43aab6cdbf0f80d51f",
+    "url": "/React-Review-Slider-Project/static/css/main.fca59e7c.chunk.css"
+  },
+  {
+    "revision": "e1c4e21724a78a9e8542",
+    "url": "/React-Review-Slider-Project/static/js/2.2c3ffba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Review-Slider-Project/static/js/2.2c3ffba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc43aab6cdbf0f80d51f",
+    "url": "/React-Review-Slider-Project/static/js/main.69c6497f.chunk.js"
+  },
+  {
+    "revision": "113e0beaebfafb6d1a1f",
+    "url": "/React-Review-Slider-Project/static/js/runtime-main.ae827e23.js"
+  }
+]);
